f5be37fbb43dc5c121218baaac7bf3936d01ab09
Md Rifat Rahman <rmdrifat547@gmail.com>
2024-11-08 20:24:12 +0600

    Android app demo.

--- app/src/main/java/com/android/developerchoice/SplashActivity.java
+++ app/src/main/java/com/android/developerchoice/SplashActivity.java
@@ -1,24 +1,32 @@
 package com.android.developerchoice;
 
+import android.content.Intent;
 import android.os.Bundle;
-
-import androidx.activity.EdgeToEdge;
+import android.os.Handler;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 public class SplashActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_splash);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+
+        ImageView logoImage = findViewById(R.id.logo_image);
+        Animation fadeInAnimation = AnimationUtils.loadAnimation(this, R.anim.fade_in);
+        logoImage.startAnimation(fadeInAnimation);
+
+        // Add delay to start main activity after the splash screen
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                // Intent to open main activity
+                startActivity(new Intent(SplashActivity.this, MainActivity.class));
+                finish();
+            }
+        }, 3000); // Delay for 3 seconds
     }
 }
