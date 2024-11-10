6cdfc09e56150ca0cdc593edfeb539f96c7f6797
Md Rifat Rahman <rmdrifat547@gmail.com>
2024-11-10 13:16:28 +0600

    version 1.0.5

--- app/src/main/java/com/android/developerchoice/LoginActivity.java
+++ app/src/main/java/com/android/developerchoice/LoginActivity.java
@@ -1,12 +1,22 @@
 package com.android.developerchoice;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -15,10 +25,72 @@ public class LoginActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_login);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.activity_login), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+
+
+        EditText emailField = findViewById(R.id.emailField);
+        EditText passwordField = findViewById(R.id.passwordField);
+        Button loginButton = findViewById(R.id.loginButton);
+        TextView forgotPassword = findViewById(R.id.forgotPassword);
+        TextView signUpText = findViewById(R.id.signUpText);
+        FirebaseAuth mAuth = FirebaseAuth.getInstance();
+
+
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String userEmail = String.valueOf(emailField.getText());
+                String userPassword = String.valueOf(passwordField.getText());
+
+                if (userEmail.isEmpty()) {
+                    emailField.setError("Email is required");
+                    emailField.requestFocus();
+                    return;
+                }
+
+                if (userPassword.isEmpty()) {
+                    passwordField.setError("Password is required");
+                    passwordField.requestFocus();
+                }
+
+                // Use FirebaseAuth to sign in with email and password
+                mAuth.signInWithEmailAndPassword(userEmail, userPassword)
+                        .addOnCompleteListener(LoginActivity.this, task -> {
+                            if (task.isSuccessful()) {
+                                // Sign-in success
+                                FirebaseUser user = mAuth.getCurrentUser();
+                                Toast.makeText(LoginActivity.this, "Login successful!", Toast.LENGTH_SHORT).show();
+
+                                // Navigate to main activity or dashboard
+                                Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                                startActivity(intent);
+                                finish();
+                            } else {
+                                // Sign-in failure
+                                Toast.makeText(LoginActivity.this, "Authentication failed: " + task.getException().getMessage(),
+                                        Toast.LENGTH_SHORT).show();
+                            }
+                        });
+            }
+        });
+
+        forgotPassword.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(LoginActivity.this, "In coming.", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        signUpText.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(LoginActivity.this, SingupActivity.class));
+                finish();
+            }
         });
     }
 }
--- app/src/main/java/com/android/developerchoice/SingupActivity.java
+++ app/src/main/java/com/android/developerchoice/SingupActivity.java
@@ -1,12 +1,21 @@
 package com.android.developerchoice;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class SingupActivity extends AppCompatActivity {
 
@@ -15,10 +24,70 @@ public class SingupActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_singup);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.activity_singup), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+
+        EditText nameField = findViewById(R.id.nameField);
+        EditText emailField = findViewById(R.id.emailField);
+        EditText passwordField = findViewById(R.id.passwordField);
+        Button singupButton = findViewById(R.id.singupButton);
+        TextView loginText = findViewById(R.id.loginText);
+
+
+        singupButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String userName = String.valueOf(nameField.getText());
+                String userEmail = String.valueOf(emailField.getText());
+                String userPassword = String.valueOf(passwordField.getText());
+                FirebaseAuth mAuth = FirebaseAuth.getInstance();
+
+                if (userEmail.isEmpty()) {
+                    emailField.setError("Email is required");
+                    emailField.requestFocus();
+                    return;
+                }
+
+                if (userPassword.isEmpty()) {
+                    passwordField.setError("Password is required");
+                    passwordField.requestFocus();
+                    return;
+                }
+
+                if (userName.isEmpty()) {
+                    nameField.setError("Password is required");
+                    nameField.requestFocus();
+                    return;
+                }
+
+                mAuth.createUserWithEmailAndPassword(userEmail, userPassword)
+                        .addOnCompleteListener(SingupActivity.this, task -> {
+                            if (task.isSuccessful()) {
+                                // User creation success
+                                FirebaseUser user = mAuth.getCurrentUser();
+                                Toast.makeText(SingupActivity.this, "User created successfully!", Toast.LENGTH_SHORT).show();
+                                // Navigate to another activity if needed
+                                Intent intent = new Intent(SingupActivity.this, MainActivity.class);
+                                startActivity(intent);
+                                finish();
+                            } else {
+                                // If sign up fails, display a message to the user.
+                                Toast.makeText(SingupActivity.this, "Authentication failed: " + task.getException().getMessage(),
+                                        Toast.LENGTH_SHORT).show();
+                            }
+                        });
+            }
+        });
+
+        loginText.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(SingupActivity.this, LoginActivity.class));
+                finish();
+            }
         });
     }
 }
