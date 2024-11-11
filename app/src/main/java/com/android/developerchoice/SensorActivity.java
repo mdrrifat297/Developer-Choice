df88d2fc477e7b5dcea227ead9f420b31d6583c0
Md Rifat Rahman <rmdrifat547@gmail.com>
2024-11-11 17:25:45 +0600

    version 1.1.1

--- app/src/main/java/com/android/developerchoice/SensorActivity.java
+++ app/src/main/java/com/android/developerchoice/SensorActivity.java
@@ -1,24 +1,125 @@
 package com.android.developerchoice;
 
+import android.content.Context;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.os.Bundle;
-
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class SensorActivity extends AppCompatActivity {
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.TextView;
+
+import com.android.developerchoice.R;
+
+public class SensorActivity extends AppCompatActivity implements SensorEventListener {
+
+    private SensorManager sensorManager;
+    private Sensor accelerometer, proximitySensor, lightSensor;
+    private TextView accelerometerData, proximityData, lightData;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_sensor);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+
+        ImageButton goBack = findViewById(R.id.goBack);
+
+
+        goBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+
+
+        // Initialize TextViews
+        accelerometerData = findViewById(R.id.accelerometerData);
+        proximityData = findViewById(R.id.proximityData);
+        lightData = findViewById(R.id.lightData);
+
+        // Initialize SensorManager
+        sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+
+        // Initialize and register Accelerometer
+        if (sensorManager != null) {
+            accelerometer = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+            if (accelerometer != null) {
+                sensorManager.registerListener(this, accelerometer, SensorManager.SENSOR_DELAY_NORMAL);
+            }
+
+            // Initialize and register Proximity Sensor
+            proximitySensor = sensorManager.getDefaultSensor(Sensor.TYPE_PROXIMITY);
+            if (proximitySensor != null) {
+                sensorManager.registerListener(this, proximitySensor, SensorManager.SENSOR_DELAY_NORMAL);
+            }
+
+            // Initialize and register Light Sensor
+            lightSensor = sensorManager.getDefaultSensor(Sensor.TYPE_LIGHT);
+            if (lightSensor != null) {
+                sensorManager.registerListener(this, lightSensor, SensorManager.SENSOR_DELAY_NORMAL);
+            }
+        }
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+        switch (event.sensor.getType()) {
+            case Sensor.TYPE_ACCELEROMETER:
+                float x = event.values[0];
+                float y = event.values[1];
+                float z = event.values[2];
+                accelerometerData.setText("Accelerometer\nX: " + x + "\nY: " + y + "\nZ: " + z);
+                break;
+
+            case Sensor.TYPE_PROXIMITY:
+                float proximity = event.values[0];
+                proximityData.setText("Proximity\nDistance: " + proximity + " cm");
+                break;
+
+            case Sensor.TYPE_LIGHT:
+                float light = event.values[0];
+                lightData.setText("Light Level\n" + light + " lx");
+                break;
+        }
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+        // Not used in this example
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        // Unregister the listener to save battery when the activity is not visible
+        sensorManager.unregisterListener(this);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        // Re-register sensors when the activity is resumed
+        if (accelerometer != null) {
+            sensorManager.registerListener(this, accelerometer, SensorManager.SENSOR_DELAY_NORMAL);
+        }
+        if (proximitySensor != null) {
+            sensorManager.registerListener(this, proximitySensor, SensorManager.SENSOR_DELAY_NORMAL);
+        }
+        if (lightSensor != null) {
+            sensorManager.registerListener(this, lightSensor, SensorManager.SENSOR_DELAY_NORMAL);
+        }
     }
 }
